cc2784db1cb4d7f48d378f08a91b79638187f877
Anirudha Kashid <anirudhakashid2004@gmail.com>
2024-09-24 12:36:52 +0530

    added button styling

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import useTheme from "../context/theme";
 
-
 export default function ThemeButton() {
   const { themeMode, darkTheme, lightTheme } = useTheme();
 
@@ -15,17 +14,20 @@ export default function ThemeButton() {
   };
 
   return (
-    <label className="mt-2">
-      <input
-        type="checkbox"
-        value=""
-        onChange={toggleTheme}
-        checked={themeMode == "dark"}
-      />
-
-      <span className="ml-3 text-sm font-medium dark:text-wh text-gray-900">
+    <div className="flex items-center">
+      <label className="relative inline-block w-10 mr-2 align-middle select-none">
+        <input
+          type="checkbox"
+          onChange={toggleTheme}
+          checked={themeMode === "dark"}
+          className="toggle-checkbox absolute opacity-0 w-0 h-0"
+        />
+        <span className="toggle-slider block w-10 h-6 bg-gray-300 rounded-full cursor-pointer transition-all duration-300 ease-in-out"></span>
+        <span className="toggle-slider-dot absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition-transform duration-300 ease-in-out transform"></span>
+      </label>
+      <span className="ml-3 text-sm font-medium text-gray-900 dark:text-slate-300">
         Toggle Theme
       </span>
-    </label>
+    </div>
   );
 }
